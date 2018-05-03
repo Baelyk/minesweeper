9f3e3338f8bf4596a4f6b89a0663df3e73dc4791
Baelyk <b43lyk@gmail.com>
2018-05-02 21:25:16 -0500

    Initial minefield
    Setting up the interface of the game. Just a grid right now.

--- src/grid.rs
+++ src/grid.rs
@@ -3,45 +3,53 @@ extern crate piston_window;
 use grid::piston_window::draw_state::DrawState;
 use grid::piston_window::G2d;
 use grid::piston_window::line::Line;
+use grid::piston_window::rectangle::Rectangle;
 use grid::piston_window::types::Matrix2d;
 
-struct Cell {
+pub struct Cell {
     x: usize,
     y: usize
 }
 
-struct Grid {
+pub struct Grid {
     width: usize,
     height: usize,
     cell_width: usize,
     cell_height: usize,
     line_radius: f64,
-    line_color: [f64; 4]
+    line_color: [f32; 4],
+    background_color: [f32; 4]
 }
 
 impl Grid {
-    pub fn new (width: usize, height: usize, cell_width: usize, cell_height: usize, line_radius: f64, line_color: [f64; 4]) -> Grid {
+    pub fn new (width: usize, height: usize, cell_width: usize, cell_height: usize, line_radius: f64, line_color: [f32; 4], background_color: [f32; 4]) -> Grid {
         Grid {
             width: width,
             height: height,
             cell_width: cell_width,
             cell_height: cell_height,
             line_radius: line_radius,
-            line_color: line_color
+            line_color: line_color,
+            background_color: background_color
         }
     }
-    pub fn new_reg (size: usize, pixels: usize, line_radius: f64, line_color: [f64; 4]) -> Grid {
+    pub fn new_reg (size: usize, pixels: usize, line_radius: f64, line_color: [f32; 4], background_color: [f32; 4]) -> Grid {
         Grid {
             width: size,
             height: size,
             cell_width: pixels,
             cell_height: pixels,
             line_radius: line_radius,
-            line_color: line_color
+            line_color: line_color,
+            background_color: background_color
         }
     }
-    fn draw (&self, line: &Line, draw_state: &DrawState, transform: Matrix2d, g: &mut G2d) {
-        for x in 0..self.width {
+    pub fn draw (&self, draw_state: &DrawState, transform: Matrix2d, g: &mut G2d) {
+        let line = Line::new(self.line_color, self.line_radius);
+        let rect = Rectangle::new(self.background_color);
+        rect.draw([0.0, 0.0, (self.width * self.cell_width) as f64, (self.height * self.cell_height) as f64], draw_state, transform, g);
+
+        for x in 0..self.width + 1 {
             line.draw([
                 (x * self.cell_width) as f64,
                 0.0,
@@ -49,12 +57,34 @@ impl Grid {
                 (self.height * self.cell_height) as f64
             ], draw_state, transform, g);
         }
-        for y in 0..self.height {
+        for y in 0..self.height + 1 {
             line.draw([
                 0.0,
                 (y * self.cell_height) as f64,
                 (self.width * self.cell_width) as f64,
                 (y * self.cell_height) as f64
+            ], draw_state, transform, g);
+        }
+    }
+    pub fn draw_offset (&self, x_off: f64, y_off: f64, draw_state: &DrawState, transform: Matrix2d, g: &mut G2d) {
+        let line = Line::new(self.line_color, self.line_radius);
+        let rect = Rectangle::new(self.background_color);
+        rect.draw([0.0 + x_off, 0.0 + y_off, (self.width * self.cell_width) as f64, (self.height * self.cell_height) as f64], draw_state, transform, g);
+
+        for x in 0..self.width + 1 {
+            line.draw([
+                (x * self.cell_width) as f64 + x_off,
+                0.0 + y_off,
+                (x * self.cell_width) as f64 + x_off,
+                (self.height * self.cell_height) as f64 + y_off
+            ], draw_state, transform, g);
+        }
+        for y in 0..self.height + 1 {
+            line.draw([
+                0.0 + x_off,
+                (y * self.cell_height) as f64 + y_off,
+                (self.width * self.cell_width) as f64 + x_off,
+                (y * self.cell_height) as f64 + y_off
             ], draw_state, transform, g);
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,58 @@
+extern crate piston_window;
+
 mod grid;
 
+// TODO: Try to limit this from a * to just the things we need
+use piston_window::*;
+use grid::Grid;
+
+const TILE_SIZE: usize = 50;
+const GRID_X: usize = 10;
+const GRID_Y: usize = 10;
+const GRID_RADIUS: f64 = 1.0;
+const GRID_COLOR: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+const BACK_COLOR: [f32; 4] = [0.55, 0.7, 0.95, 1.0];
+const GRID_OFFSET: usize = 100;
+
+// TODO: Hover color, clicked color (held down but not released)
+
 fn main() {
-    println!("Hello, world!");
+    // Title: Minesweeper
+    // GRID_X * TILE_SIZE by GRID_Y * TILE_SIZE
+    // esc to exit
+    // not resizable
+    let mut window: PistonWindow = WindowSettings::new("Minesweeper", [GRID_X as u32 * TILE_SIZE as u32 + GRID_OFFSET as u32 * 2 as u32, GRID_Y as u32 * TILE_SIZE as u32 + GRID_OFFSET as u32 * 2 as u32])
+        .exit_on_esc(true)
+        .resizable(false)
+        .build()
+        .unwrap();
+    // Set the ups to 1 by default, but allow it to be changed
+    // TODO: determine if ups needs to be mutable
+    let mut ups = 1;
+    let grid = Grid::new(GRID_X, GRID_Y, TILE_SIZE, TILE_SIZE, GRID_RADIUS, GRID_COLOR, BACK_COLOR);
+
+    window.events.set_ups(ups);
+
+    while let Some(event) = window.next() {
+        // Draw the grid
+        window.draw_2d(&event, |c, g| {
+            clear([1.0; 4], g); // [1.0; 4] is short for [1.0, 1.0, 1.0, 1.0] which is white
+            // Draw the grid with a Noned &DrawState
+            grid.draw_offset(GRID_OFFSET as f64, GRID_OFFSET as f64, &DrawState {
+                scissor: None,
+                stencil: None,
+                blend: None
+            }, c.transform, g);
+        });
+
+        // Let the ups be changed by the up and down arrow keys
+        if let Some(Button::Keyboard(Key::Up)) = event.press_args() {
+            ups += 1;
+            window.events.set_ups(ups);
+        }
+        if let Some(Button::Keyboard(Key::Down)) = event.press_args() {
+            if ups > 1 { ups -= 1; }
+            window.events.set_ups(ups);
+        }
+    }
 }
